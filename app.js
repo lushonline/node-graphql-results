9f53e3b3061fc746e008c38e64e2a2b09311d541
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-06-02 11:33:33 +0100

    Add Docker

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-
 const schema = require('./src/schema/schema');
 
 const app = express();
+
+require('dotenv').config();
 
 app.use(
   '/graphql',
@@ -13,11 +14,14 @@ app.use(
   })
 );
 
-const PORT = process.PORT || 4000;
+const PORT = process.env.PORT || 4000;
+const DATABASE_URL = process.env.DATABASE_URL || '';
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Now app is running on port ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`Database Connection String: ${DATABASE_URL}`);
 });
 
 module.exports = {
